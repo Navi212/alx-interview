e1e6bfb9e22c3f70ee5e9564ecda85628fbc27dc
Joseph Nweke <josephchristo615@gmail.com>
2023-12-14 17:41:50 +0100

    Updates task 0 file

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,45 +1,25 @@
 #!/usr/bin/python3
 """
-Calculates the minimum number of operations needed to achieve 'n' characters
-by using dynamic programming.
+This is the 0-minoperations.py module.
 """
 
 
 def minOperations(n):
     """
-    Calculates the minimum number of operations needed to achieve 'n'
-    characters.
-
-    Args:
-    - n (int): The target number of characters.
-
-    Returns:
-    - int: The minimum number of operations required.
+    find the prime factorization of n
     """
 
-    if n <= 1:
+    if (n <= 0):
         return 0
 
-    # Create a list to store the minimum operations needed for each count of
-    # characters
-    operations_needed = [0] * (n + 1)
-
-    # Iterate through the range of 2 to n to calculate minimum operations for
-    # each count
-    for i in range(2, n + 1):
-        # Initialize the number of operations for i characters to i
-        # (maximum number of operations)
-        operations_needed[i] = i
-
-        # Iterate through possible factors j of i to find the minimum
-        # operations
-        for j in range(2, i // 2 + 1):
-            # Check if j is a factor of i
-            if i % j == 0:
-                # Update operations_needed[i] with the minimum operations found
-                # so far
-                operations_needed[i] = min(
-                        operations_needed[i], operations_needed[j] + i // j)
-
-    # Return the minimum operations needed to achieve n characters
-    return operations_needed[n]
+    prime_fact = []
+    while n % 2 == 0:
+        prime_fact.append(2)
+        n = n / 2
+    for num in range(3, int(n ** 0.5) + 1, 2):
+        while n % num == 0:
+            prime_fact.append(num)
+            n = n / num
+    if n > 2:
+        prime_fact.append(n)
+    return int(sum(prime_fact))
